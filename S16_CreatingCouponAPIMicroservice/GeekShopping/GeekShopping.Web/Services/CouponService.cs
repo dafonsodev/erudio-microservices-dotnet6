ab7670e626edba4f01882b88e82043623c209bd5
Daniel Afonso <danielafonso@outlook.com>
2023-08-10 16:35:59 +0100

    Creating CouponAPI Microservice

---
+++ S16_CreatingCouponAPIMicroservice/GeekShopping/GeekShopping.Web/Services/CouponService.cs
@@ -0,0 +1,15 @@
+﻿using GeekShopping.Web.Services.IServices;
+
+namespace GeekShopping.Web.Services
+{
+    public class CouponService : ICouponService
+    {
+        private readonly HttpClient _client;
+        public const string BasePath = "api/v1/cart";
+
+        public CouponService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+    }
+}
