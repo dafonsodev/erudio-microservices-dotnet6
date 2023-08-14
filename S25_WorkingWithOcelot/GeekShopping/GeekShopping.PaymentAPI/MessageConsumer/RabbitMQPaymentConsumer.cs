02e5b466813a4a79c53770b114001362a5f6c039
Daniel Afonso <danielafonso@outlook.com>
2023-08-14 16:17:13 +0100

    Working With Ocelot

---
+++ S25_WorkingWithOcelot/GeekShopping/GeekShopping.PaymentAPI/MessageConsumer/RabbitMQPaymentConsumer.cs
@@ -0,0 +1,72 @@
+﻿using GeekShopping.PaymentAPI.Messages;
+using GeekShopping.PaymentAPI.RabbitMQSender;
+using GeekShopping.PaymentProcessor;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace GeekShopping.PaymentAPI.MessageConsumer
+{
+    public class RabbitMQPaymentConsumer : BackgroundService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly IRabbitMQMessageSender _rabbitMQMessageSender;
+        private readonly IProcessPayment _processPayment;
+
+        public RabbitMQPaymentConsumer(IRabbitMQMessageSender rabbitMQMessageSender, 
+            IProcessPayment processPayment)
+        {
+            _rabbitMQMessageSender = rabbitMQMessageSender;
+            _processPayment = processPayment;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "orderpaymentprocessqueue", false, false, false, arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (chanel, evt) =>
+            {
+                var content = Encoding.UTF8.GetString(evt.Body.ToArray());
+                PaymentMessage paymentMessage = JsonSerializer.Deserialize<PaymentMessage>(content);
+                ProcessPayment(paymentMessage).GetAwaiter().GetResult();
+                _channel.BasicAck(evt.DeliveryTag, false);
+            };
+            _channel.BasicConsume("orderpaymentprocessqueue", false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private async Task ProcessPayment(PaymentMessage paymentMessage)
+        {
+            var result = _processPayment.PaymentProcessor();
+
+            UpdatePaymentResultMessage updatePayment = new()
+            {
+                Email = paymentMessage.Email,
+                OrderId = paymentMessage.OrderId,
+                Status = result
+            };
+
+            try
+            {
+                _rabbitMQMessageSender.SendMessage(updatePayment);
+            }
+            catch (Exception)
+            {
+                //Log
+                throw;
+            }
+        }
+    }
+}
